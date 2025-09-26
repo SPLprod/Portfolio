4b02ee782c4f796e9050032ebacfa40e1562e407
SPLprod <lilidanjou@gmail.com>
2025-09-26 16:21:33 +0200

    ver 1.0.1 (tentative ajustement responsive)

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,14 +1,4 @@
 const mailForm = document.querySelector("form");
-const emailInput = document.getElementById("mail");
-const objectInput = document.getElementById("object");
-const messageInput = document.getElementById("msg");
-
-console.log(mailForm);
-console.log(emailInput);
-console.log(objectInput);
-console.log(messageInput);
-
-
 
 mailForm.addEventListener("submit", function (e) {
     e.preventDefault();
